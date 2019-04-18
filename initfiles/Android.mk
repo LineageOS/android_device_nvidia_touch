16c5db4fd1fdd52aece2dc7ecede643819c3861a
Aaron Kling <webgeek1234@gmail.com>
2019-04-18 14:11:27 -0500

    Add raydium support
    Change-Id: I19a5b4ff7c43925f4721442506d4ef02d73bb91b

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -7,3 +7,18 @@ LOCAL_SRC_FILES            := init.cal.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.ray_touch.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.ray_touch.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.ray_touch.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.ray_touch.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
