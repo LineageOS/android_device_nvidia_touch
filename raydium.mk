e2f2df173e026901db8a85cefb2c2414fd036c39
Aaron Kling <webgeek1234@gmail.com>
2020-12-28 20:12:50 -0600

    raydium: Update recovery lib copy paths for Q
    Change-Id: I5b1aaa25b45d5b40ff4b2e8a0348c960739f5a8c

--- raydium.mk
+++ raydium.mk
@@ -31,9 +31,9 @@ TARGET_RECOVERY_DEVICE_MODULES := rm-wrapper
 # Raydium userspace is 32-bit, need to explicitly include the libs for recovery when 64-bit
 ifeq ($(TARGET_TEGRA_VERSION),t210)
     PRODUCT_COPY_FILES += \
-        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/libc.so:recovery/root/sbin32/libc.so \
-        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/libdl.so:recovery/root/sbin32/libdl.so \
-        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/libm.so:recovery/root/sbin32/libm.so \
+        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/apex/com.android.runtime.release/lib/bionic/libc.so:recovery/root/sbin32/libc.so \
+        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/apex/com.android.runtime.release/lib/bionic/libdl.so:recovery/root/sbin32/libdl.so \
+        $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/apex/com.android.runtime.release/lib/bionic/libm.so:recovery/root/sbin32/libm.so \
         $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/liblog.so:recovery/root/sbin32/liblog.so \
         $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/libc++.so:recovery/root/sbin32/libc++.so \
         $(OUT_DIR)/target/product/$(LINEAGE_BUILD)/system/lib/libstdc++.so:recovery/root/sbin32/libstdc++.so
